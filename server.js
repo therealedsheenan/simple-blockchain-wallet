d5eccef6b20f410158f089cf9085d0387d9f6419
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-19 21:56:48 +0900

    simple node server

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express')
+const app = express()
+const BitGoJS = require('bitgo')
+const bodyParser = require('body-parser')
+
+// sample address: 2NEywWXCBcPBkDvDu5C3qRhdXdA65EoQkzi
+const WALLET_ADDRESS = '2NEywWXCBcPBkDvDu5C3qRhdXdA65EoQkzi'
+
+// body parser
+app.use(bodyParser.json())
+
+const API = '/api/v1'
+
+const ACCESS_TOKEN = process.env.ACCESS_TOKEN || ''
+const bitgo = new BitGoJS.BitGo({env: 'test', accessToken: ACCESS_TOKEN})
+
+app.get(`${API}/`, (req, res) => res.send('Server is alive!'))
+
+// authentication
+app.get(`${API}/auth`, (req, res) =>
+  bitgo.session({})
+    .then(response => res.json(response))
+    .catch(error => res.json({ error: error }))
+)
+
+// create wallet
+app.post(`${API}/create`, (req, res, next) => {
+  const { label, pass } = req.body
+  if (!label || pass) {
+    next('Invalid data')
+  }
+  bitgo.wallets().createWalletWithKeychains({'passphrase': pass, 'label': label}, (err, result) => {
+    if (err) {
+      console.dir(err)
+      next('Error creating wallet!')
+    }
+    return res.json({
+      walletId: result.wallet.id(),
+      data: result.wallet.wallet
+    })
+  })
+})
+
+// get wallet balance
+app.get(`${API}/balance`, (req, res, next) => {
+  const { address } = req.body
+  if (!address) {
+    next('unable to fetch data.')
+  }
+  bitgo.wallets().get({type: 'bitcoin', id: WALLET_ADDRESS}, (err, wallet) => {
+    if (err) { console.log(err); process.exit(-1) }
+    return res.json({
+      balance: wallet.balance()
+    })
+  })
+})
+
+// sending bitcoin
+app.post(`${API}/send`, (req, res, next) => {
+  const { walletId, walletPass, destination, amount } = req.body
+  if (!walletId || !walletPass || !destination || !amount) {
+    next('unable to send bitcoin.')
+  }
+  bitgo.wallets().get({id: walletId}, (err, wallet) => {
+    if (err) {
+      console.log(err)
+      next(err)
+    }
+
+    const walletBalance = (wallet.balance() / 1e8).toFixed(4)
+
+    wallet.sendCoins({ address: destination, amount: amount, walletPassphrase: walletPass, minConfirms: 0 },
+      (err, result) => {
+        if (err) {
+          console.log(err)
+          next(err)
+        }
+
+        return res.json({
+          ...result,
+          balance: walletBalance
+        })
+      }
+    );
+  });
+})
+
+app.listen(3001, () => console.log('Server listening on port 3001!'))
