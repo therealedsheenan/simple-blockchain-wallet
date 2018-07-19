10c4de8927b99c11167d00a941d9d5c34003192f
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-19 23:04:08 +0900

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+import App from "./App";
+import store, { history } from "./store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
+
 registerServiceWorker();
---
+++ src/reducers.js
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux";
+
+const sampleReducer = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ sampleReducer });
