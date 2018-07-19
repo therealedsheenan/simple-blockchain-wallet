c38270f68e2b7844042e27a9193997ff000464cb
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-19 23:41:39 +0900

    react setup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { ConnectedRouter } from "connected-react-router";
+import "zent/css/index.css";
 
 import registerServiceWorker from "./registerServiceWorker";
-import "./index.css";
+
 import App from "./App";
 import store, { history } from "./store";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>
