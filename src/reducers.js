d85689fdd4f900bc8c3d8ca34838f96afdb31de4
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-25 00:42:15 +0900

    add auth reducer

--- src/reducers.js
+++ src/reducers.js
@@ -2,5 +2,6 @@ import { combineReducers } from "redux";
 
 import wallet from "./modules/wallet/reducer";
 import walletList from "./modules/walletList/reducer";
+import auth from "./modules/auth/reducer";
 
-export default combineReducers({ wallet, walletList });
+export default combineReducers({ wallet, walletList, auth });
