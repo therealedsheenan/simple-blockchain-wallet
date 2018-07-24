751b77d61fc0e6668ae0bc7fd9b6217fce29cafa
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-25 00:42:30 +0900

    axios api calls as service

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,26 @@ import axios from "axios";
 
 const BASE_API_URL = "http://localhost:8000/api/v1";
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
   timeout: 10000
 });
+
+const Api = (options = {}) => {
+  return axiosInstance(options)
+    .then(response => {
+      // other successful response
+      return response.json().then(json => {
+        return { json, response };
+      });
+    })
+    .then(({ json, response }) => {
+      if (!response.ok) {
+        return Promise.reject(json);
+      }
+      return json;
+    })
+    .then(response => ({ response }), error => ({ error }));
+};
+
+export default Api;
