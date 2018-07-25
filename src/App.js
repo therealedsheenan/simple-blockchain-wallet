fa2b55878e2b14ce1493af0a95dcd2e91220945c
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-25 23:08:38 +0900

    navigation styling and auth sync

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
 import Home from "./pages/Home";
 import Wallet from "./pages/Wallet";
+
+const Private = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        rest.auth.data.isAuthenticated ? <Component {...props} /> : null
+      }
+    />
+  );
+};
+
+const PrivateRoute = connect(
+  ({ auth }) => ({ auth }),
+  null
+)(Private);
 
 const App = () => (
   <main className="App">
     <Route exact path="/" component={Home} />
     <Route exact path="/wallet" component={Wallet} />
+    <PrivateRoute exact path="/" component={() => <div>private,,,</div>} />
   </main>
 );
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,50 @@
 import React, { Component } from "react";
-import { Layout } from "zent";
+import { Layout, Button } from "zent";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { getToken, getUser } from "../utils";
+import { postUnauthRequest } from "../modules/auth/actions";
 
 const { Row, Col } = Layout;
 
 class Navigation extends Component {
+  static propTypes = {
+    postUnauthRequest: PropTypes.func.isRequired
+  };
+  state = {
+    user: {}
+  };
+
+  componentWillMount() {
+    if (getToken()) {
+      this.setState({
+        user: {
+          ...getUser(),
+          isAuthenticated: !!getUser()
+        }
+      });
+    }
+
+    // logout here
+  }
   render() {
+    const { state, props } = this;
     return (
       <div className="Navigation theme-primary-2">
         <Row>
-          <Col>
+          <Col span={12}>
             <h1>Simple blockchain wallet</h1>
+          </Col>
+          <Col span={12}>
+            {state.user.isAuthenticated && (
+              <div className="Navigation-user">
+                <span>{state.user.username}</span>
+                <Button type="danger" outline onClick={props.postUnauthRequest}>
+                  Logout
+                </Button>
+              </div>
+            )}
           </Col>
         </Row>
       </div>
@@ -17,4 +52,9 @@ class Navigation extends Component {
   }
 }
 
-export default Navigation;
+export default connect(
+  ({ auth }) => ({ auth }),
+  dispatch => ({
+    postUnauthRequest: () => dispatch(postUnauthRequest())
+  })
+)(Navigation);
