26bf1b2126ee8bfd400eb894171fff4eafd0afff
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-26 23:31:01 +0900

    add new routing pages

--- src/App.js
+++ src/App.js
@@ -1,31 +1,23 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
+import { Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Wallets from "./pages/Wallets";
 import Wallet from "./pages/Wallet";
+import PageError from "./pages/PageError";
 
-const Private = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        rest.auth.data.isAuthenticated ? <Component {...props} /> : null
-      }
-    />
-  );
-};
-
-const PrivateRoute = connect(
-  ({ auth }) => ({ auth }),
-  null
-)(Private);
+import Bitcoin from "./pages/Bitcoin";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => (
   <main className="App">
-    <Route exact path="/" component={Home} />
-    <Route exact path="/wallet" component={Wallet} />
-    <PrivateRoute exact path="/" component={() => <div>private,,,</div>} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <PrivateRoute exact path="/wallets" component={Wallets} />
+      <PrivateRoute exact path="/wallets/send" component={Bitcoin} />
+      <PrivateRoute exact path="/wallets/:id" component={Wallet} />
+      <Route component={PageError} />
+    </Switch>
   </main>
 );
 
