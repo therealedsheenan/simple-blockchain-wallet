a4fa6b7ac0189f6a47cf1450ba3dfe19bd7890e4
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-07-29 15:14:09 +0900

    api export

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const BASE_API_URL = "http://localhost:8000/api/v1";
 
-const axiosInstance = axios.create({
+export const axiosInstance = axios.create({
   baseURL: BASE_API_URL,
   timeout: 10000
 });
