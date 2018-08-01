8bb4dd9705be06ba761f1cb3a69e0f72fe44851f
Sheenan Tenepre <sheenantenepre@gmail.com>
2018-08-01 15:22:29 +0900

    Add wallets mock data

--- src/__mocks__/wallet.js
+++ src/__mocks__/wallet.js
@@ -5,7 +5,8 @@ export const walletId = "5b4f5bbc6b0ca18d03e4d0f1c532aec6";
 export const walletData = [
   {
     _wallet: {
-      id: walletId
+      id: walletId,
+      label: "Label1"
     }
   }
 ];
